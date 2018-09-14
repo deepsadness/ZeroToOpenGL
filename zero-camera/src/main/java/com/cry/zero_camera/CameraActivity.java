83e60891e2094f5059016173fb9ea5357920cd07
deep_sadness <a295756092@gmail.com>
2018-09-14 11:27:08 +0800

    use rx-permission

--- zero-camera/src/main/java/com/cry/zero_camera/CameraActivity.java
+++ zero-camera/src/main/java/com/cry/zero_camera/CameraActivity.java
@@ -1,6 +1,7 @@
 package com.cry.zero_camera;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -17,11 +18,13 @@ import android.widget.FrameLayout;
 import android.widget.Toast;
 
 import com.cry.zero_common.permission.ConfirmationDialogFragment;
+import com.tbruyelle.rxpermissions2.RxPermissions;
 
 public class CameraActivity extends AppCompatActivity {
     private static final int REQUEST_CAMERA_PERMISSION = 2;
     private FrameLayout mContainer;
     public CameraView mCameraView;
+    private RxPermissions rxPermissions;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +36,7 @@ public class CameraActivity extends AppCompatActivity {
         setContentView(R.layout.activity_camera);
         mContainer = (FrameLayout) findViewById(R.id.container);
 
+        rxPermissions = new RxPermissions(this);
     }
 
     @Override
@@ -43,51 +47,27 @@ public class CameraActivity extends AppCompatActivity {
         }
     }
 
+    @SuppressLint("CheckResult")
     @Override
     protected void onResume() {
         super.onResume();
         if (mCameraView != null) {
             mCameraView.onResume();
         } else {
-            //申请权限
-            requestPermission();
-        }
-    }
-
-    private void requestPermission() {
-        if ((ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
-                && (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)) {
-            Toast.makeText(this, "已经获取权限", Toast.LENGTH_SHORT).show();
-            startCamera();
-        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
-            ConfirmationDialogFragment
-                    .newInstance("camera_permission_confirmation",
-                            new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                            REQUEST_CAMERA_PERMISSION,
-                            "camera_permission_not_granted")
-                    .show(getSupportFragmentManager(), "FRAGMENT_DIALOG");
-        } else {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                    REQUEST_CAMERA_PERMISSION);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CAMERA_PERMISSION:
-//                if (permissions.length != 1 || grantResults.length != 1) {
-//                    throw new RuntimeException("Error on requesting camera permission.");
-//                }
-                if (grantResults[0] != PackageManager.PERMISSION_GRANTED && grantResults[1] != PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "camera_permission_not_granted",
-                            Toast.LENGTH_SHORT).show();
-                    finish();
-                }
-                // No need to start camera here; it is handled by onResume
-                startCamera();
-                break;
+            rxPermissions
+                    .request(Manifest.permission.CAMERA)
+                    .subscribe(granted -> {
+                        if (granted) {
+                            startCamera();
+                        } else {
+                            ConfirmationDialogFragment
+                                    .newInstance("camera_permission_confirmation",
+                                            new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                            REQUEST_CAMERA_PERMISSION,
+                                            "camera_permission_not_granted")
+                                    .show(getSupportFragmentManager(), "FRAGMENT_DIALOG");
+                        }
+                    });
         }
     }
 
